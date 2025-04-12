0ad15f902dea2e9610e069c8b33870a118e3609e
Sanders-57 <a.s.gusakov@yandex.ru>
2025-04-12 14:05:54 +0300

    Сверстал секцию с карточками бургеров и прочьего

---
+++ src/Components/ListItem.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// Список с карточками 
+const List = styled.ul`
+  display: flex;
+  justydy-content: space-around;
+  flex-wrap: wrap;
+
+`
+// Карточка 
+const Item = styled.li`
+  position: relative;
+  width: 400px;
+  height: 155px;
+  background-image: ${({img}) => `url(${img})`};
+  background-position: center;
+  background-size: cover;
+  margin-top: 30px;
+  margin-right: 30px;
+  padding: 15px;
+  color: white;
+  z-index:1;
+  box-shadow: 0 0 0 rgba(0, 0, 0, 0);
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  cursor: pointer;
+  transform: scale(1); /* начальный масштаб */
+  &:after{
+    content: '';
+    position: absolute;
+    top:0;
+    bottom:0;
+    right:0;
+    left:0;
+    background-color: black;
+    opacity: 50%;
+    z-index:-1;
+  }
+    &:hover{
+      transform: scale(1.03); /* легкое увеличение */
+      box-shadow: 0 8px 20px rgba(0, 0, 0, 0.5); /* лёгкая тень */
+    }
+`
+
+
+export const ListItem = ({itemList}) => (
+  <List>
+    {itemList.map(item => (
+      <Item key={item.id} img={item.img}>        
+        <p>{item.name}</p>
+        <p>{item.price.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'})}</p>
+      </Item>
+    ))}
+  </List>
+)
